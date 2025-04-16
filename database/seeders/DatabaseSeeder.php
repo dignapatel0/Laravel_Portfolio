0d9f56e0e1c1c05a2b0d664531624683f6726a97
Digna Patel <dignapatel0806@gmail.com>
2025-04-16 17:44:38 -0400

    Add initial code

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Type;
+use App\Models\Project;
+use App\Models\Skill;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        User::truncate();
+        Type::truncate();
+        Project::truncate();
+        Skill::truncate();
+        
+        User::factory()->count(2)->create();
+        Type::factory()->count(3)->create();
+        Project::factory()->count(4)->create();
+        Skill::factory()->count(5)->create();  
+    }
+}
---
+++ resources/views/skills/list.blade.php
@@ -0,0 +1,44 @@
+@extends ('layout.console')
+
+@section ('content')
+
+<section class="w3-padding">
+
+    <h2>Manage Skills</h2>
+
+    <table class="w3-table w3-stripped w3-bordered w3-margin-bottom">
+        <tr class="w3-red">
+            <th></th>
+            <th>Title</th>
+            <th>URL</th>
+            <th>Created</th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+        @foreach ($skills as $skill)
+            <tr>
+                <td>
+                    @if ($skill->image)
+                        <img src="{{asset('storage/'.$skill->image)}}" width="200">
+                    @endif
+                </td>
+                <td>{{$skill->title}}</td>
+                <td>
+                    <a href="{{$skill->url}}">
+                        {{$skill->url}}
+                    </a>
+                </td>
+                <td>{{$skill->created_at->format('M j, Y')}}</td>
+                <td><a href="/console/skills/image/{{$skill->id}}">Image</a></td>
+                <td><a href="/console/skills/edit/{{$skill->id}}">Edit</a></td>
+                <td><a href="/console/skills/delete/{{$skill->id}}">Delete</a></td>
+            </tr>
+        @endforeach
+    </table>
+
+    <a href="/console/skills/add" class="w3-button w3-green">New Skill</a>
+
+</section>
+
+@endsection
